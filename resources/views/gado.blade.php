1ed7e99e3adf76bc1b9b8aba9c573684414a0cae
ImamWaliyuddin <imamwaliyuddinrabbani@gmail.com>
2022-05-24 13:55:41 +0700

    Update gado.blade.php

--- resources/views/gado.blade.php
+++ resources/views/gado.blade.php
@@ -34,7 +34,52 @@
         <ul style="list-style-type:none;float:left ">
             <li><h1 style="font-weight:900">Gado-gado</h1> </li>
             <li><h3>Alat dan Bahan</h3> </li>
+            <p >
+            <ul style="column-count:2;column-gap:30px;">
+              <li>350 g kentang, kukus, kupas, potong-potong </li>
+              <li>350 g tahu putih</li>
+              <li>350 g tempe, potong kotak 3 cm  </li> 
+              <li>7 lembar daun selada, sobek-sobek </li> 
+              <li>2 buah ketimun, iris tipis </li> 
+              <li>100 g taoge, rebus </li> 
+              <li>200 g kol, iris kasar, rebus </li> 
+              <li>Minyak, untuk menggoreng dan menumis</li> 
+              <li>Bumbu perendam tahu-tempe, aduk rata</li> 
+              <li>3 siung bawang putih, haluskan </li>
+              <li>1 sdt ketumbar bubuk</li>
+              <li>1 sdt garam</li>
+              <li>200 ml air</li>
+              <li>200 g kacang tanah kulit, goreng, haluskan</li> 
+              <li>3 lembar daun jeruk, buang tulang daunnya </li> 
+              <li>2 sdm Bango Kecap Manis </li> 
+              <li>1 sdt garam</li> 
+              <li>3 sdt gula merah</li> 
+              <li>300 ml santan encer</li> 
+              <li>300 ml air</li>
+              <li>1 sdt asam jawa, larutkan dengan 2 sdm air</li>
+              <li>5 buah cabai merah besar, buang bijinya</li>
+              <li>3 buah cabai merah keriting</li>
+              <li>5 cm kencur</li>
+              <li>4 siung bawang putih</li> 
+              <li>½ sdt terasi goreng</li> 
+              <li>6 butir telur rebus, kupas, potong 2</li> 
+              <li>2 buah tomat, potong-potong</li> 
+              <li>3 sdm bawang merah goreng</li> 
+              <li>Kerupuk</li> 
+              <li>Emping</li>
+                
+</ul></p>
+
             <li><h3>Langkah-langkah</h3> </li>
+            <ul style="list-style-type:number">
+            <li>Aduk rata tempe dan tahu bersama bumbu perendam. Sisihkan. </li>
+            <li>Panaskan minyak, goreng tahu dan tempe hingga garing dan matang. Angkat dan tiriskan. Sisihkan.</li>
+            <li>Bumbu kacang: Panaskan 3 sdm minyak, tumis bumbu halus dan daun jeruk hingga harum. <br>
+                Masukkan kacang tanah dan Bango Kecap Manis, aduk rata. Tambahkan sisa semua bahan,<br>
+                 aduk. Masak hingga matang. Angkat. Biarkan hingga tidak panas.</li>
+            <li>Tata kentang, tahu, tempe, daun selada, ketimun, taoge, dan kol di atas piring saji. <br>
+              Tuang bumbu kacang dan taburi bawang merah goreng. Sajikan dengan pelengkap. </li>
+            </ul>
         </ul>
           
         </div>
