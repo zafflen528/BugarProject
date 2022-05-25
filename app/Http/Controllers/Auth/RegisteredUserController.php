183d7ab2746247798352053dd452212ac143cd3e
ImamWaliyuddin <imamwaliyuddinrabbani@gmail.com>
2022-05-25 11:38:06 +0700

    d

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -55,9 +55,9 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
+        
 
-        return redirect(RouteServiceProvider::HOME);
+        return redirect('/login');
     }
     public function destroy()
     {
